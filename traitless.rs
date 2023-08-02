838718e1a7a2c4bf5896dde97a3d2e940f961d88
Brennan Harris <brennanharris@air.local>
2023-08-01 19:25:52 -0600

    need to figure out TOML main config because to open the window, I have to run [2m2023-08-02T01:25:46.301863Z[0m [32m INFO[0m [2mbevy_render::renderer[0m[2m:[0m AdapterInfo { name: "Apple M2", vendor: 0, device: 0, device_type: IntegratedGpu, driver: "", driver_info: "", backend: Metal } [2m2023-08-02T01:25:46.434827Z[0m [32m INFO[0m [2mbevy_winit::system[0m[2m:[0m Creating new window "Bevy App" (0v0) [2m2023-08-02T01:25:46.476898Z[0m [32m INFO[0m [2mbevy_diagnostic::system_information_diagnostics_plugin::internal[0m[2m:[0m SystemInfo { os: "MacOS 13.3 ", kernel: "22.4.0", cpu: "Apple M2", core_count: "8", memory: "16.0 GiB" } [2m2023-08-02T01:25:46.477249Z[0m [33m WARN[0m [2mbevy_ecs_ldtk::assets[0m[2m:[0m Tileset tileset cannot be loaded, it has a null relative path. [2m2023-08-02T01:25:52.647536Z[0m [32m INFO[0m [2mbevy_window::system[0m[2m:[0m No windows are open, exiting [2m2023-08-02T01:25:52.647711Z[0m [32m INFO[0m [2mbevy_winit::system[0m[2m:[0m Closing window 0v0

---
+++ traitless.rs
@@ -0,0 +1,66 @@
+// Roughly equivalent to the "basic" example, except it doesn't use the LdtkEntity convenience
+// trait. As a result, you can run this example with --no-default-features
+
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugin(LdtkPlugin)
+        .add_startup_system(setup)
+        .add_system(process_my_entity)
+        .insert_resource(LevelSelection::Index(0))
+        .run();
+}
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("my_project.ldtk"),
+        ..Default::default()
+    });
+}
+
+#[derive(Default, Component)]
+struct ComponentA;
+
+#[derive(Default, Component)]
+struct ComponentB;
+
+fn process_my_entity(
+    mut commands: Commands,
+    entity_query: Query<(Entity, &Transform, &EntityInstance), Added<EntityInstance>>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+) {
+    for (entity, transform, entity_instance) in entity_query.iter() {
+        if entity_instance.identifier == *"MyEntityIdentifier" {
+            let tileset = asset_server.load("atlas/MV Icons Complete Sheet Free - ALL.png");
+
+            if let Some(tile) = &entity_instance.tile {
+                let texture_atlas = texture_atlases.add(TextureAtlas::from_grid(
+                    tileset.clone(),
+                    Vec2::new(tile.w as f32, tile.h as f32),
+                    16,
+                    95,
+                    None,
+                    None,
+                ));
+
+                let sprite = TextureAtlasSprite {
+                    index: (tile.y / tile.h) as usize * 16 + (tile.x / tile.w) as usize,
+                    ..Default::default()
+                };
+
+                commands.entity(entity).insert(SpriteSheetBundle {
+                    texture_atlas,
+                    sprite,
+                    transform: *transform,
+                    ..Default::default()
+                });
+            }
+        }
+    }
+}
