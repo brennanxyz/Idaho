76e109fc59eb159fbda4912fbb59672505e44371
Brennan Harris <brennan@brennanharris.xyz>
2024-11-26 19:46:16 -0700

    added a blinking action indicator

--- src/misc_objects.rs
+++ src/misc_objects.rs
@@ -55,40 +55,131 @@ pub fn update_interaction_indicators(
     }
 }
 
+const CHAR_WIDTH: f32 = 6.0;
+const CHAR_HEIGHT: f32 = 10.0;
+const CHARS_PER_ROW: f32 = 10.0;
+
+fn get_char_rect(c: char) -> Rect {
+    let (row, col) = match c {
+        'A'..='J' => (0, c as u8 - b'A'),
+        'K'..='T' => (1, c as u8 - b'K'),
+        'U'..='Z' => (2, c as u8 - b'U'),
+        'a'..='j' => (3, c as u8 - b'a'),
+        'k'..='t' => (4, c as u8 - b'k'),
+        'u'..='z' => (5, c as u8 - b'u'),
+        '0'..='9' => (6, c as u8 - b'0'),
+        _ => (0, 0), // Default to first character for unknown inputs
+    };
+
+    Rect::new(
+        col as f32 * CHAR_WIDTH,
+        row as f32 * CHAR_HEIGHT,
+        (col as f32 * CHAR_WIDTH) + CHAR_WIDTH,
+        (row as f32 * CHAR_HEIGHT) + CHAR_HEIGHT,
+    )
+}
+
+#[derive(Component)]
+pub struct InteractionIndicator {
+    pub timer: Timer,
+    pub current_frame: usize,
+    pub animation_sequence: Vec<usize>,
+}
+
+impl Default for InteractionIndicator {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(0.2, TimerMode::Repeating),
+            current_frame: 0,
+            animation_sequence: vec![0, 1, 2, 3], 
+        }
+    }
+}
+
+fn animate_interaction_indicators(
+    time: Res<Time>,
+    mut query: Query<(&mut InteractionIndicator, &mut Sprite)>,
+) {
+    for (mut indicator, mut sprite) in query.iter_mut() {
+        indicator.timer.tick(time.delta());
+        if indicator.timer.just_finished() {
+            indicator.current_frame = (indicator.current_frame + 1) % indicator.animation_sequence.len();
+            let frame_index = indicator.animation_sequence[indicator.current_frame];
+            
+            // Each frame is 3x3 pixels in a 9x3 sprite sheet
+            sprite.rect = Some(Rect::new(
+                frame_index as f32 * 5.0,
+                0.0,
+                (frame_index as f32 * 5.0) + 5.0,
+                5.0
+            ));
+        }
+    }
+}
+
 pub fn spawn_interaction_indicator(
     commands: &mut Commands,
     asset_server: &AssetServer,
     character_entity: Entity,
 ) {
+    // commands.entity(character_entity).with_children(|parent| {
+    //     parent.spawn((
+    //         SpriteBundle {
+    //             sprite: Sprite {
+    //                 color: Color::BLACK,
+    //                 custom_size: Some(Vec2::new(9.0, 12.0)), // TODO: find a way to add a border radius
+    //                 ..default()
+    //             },
+    //             transform: Transform::from_xyz(0.0, 14.0, 0.0),
+    //             visibility: Visibility::Hidden,
+    //             ..default()
+    //         },
+    //         FloatingInteractionText {
+    //             offset: Vec3::new(0.0, 12.0, 0.0),
+    //             trigger_distance: 35.0,
+    //             key_prompt: "E".to_string(),
+    //         },
+    //     ));
+    //     parent.spawn((
+    //         SpriteBundle {
+    //             texture: asset_server.load("pixelorama/alphabet.png"),
+    //             sprite: Sprite {
+    //                 color: Color::WHITE,
+    //                 custom_size: Some(Vec2::new(CHAR_WIDTH, CHAR_HEIGHT)),
+    //                 rect: Some(get_char_rect('E')),
+    //                 ..default()
+    //             },
+    //             transform: Transform::from_xyz(0.5, 13.0, 1.0),
+    //             visibility: Visibility::Hidden,
+    //             // text: Text::from_section(
+    //             //     "E",
+    //             //     TextStyle {
+    //             //         font: asset_server.load("fonts/OpenSans-Regular.ttf"),
+    //             //         font_size: 12.0,
+    //             //         color: Color::WHITE,
+    //             //     },
+    //             // ),
+    //             // transform: Transform::from_xyz(0.0, 14.0, 1.0),
+    //             // visibility: Visibility::Hidden,
+    //             ..default()
+    //         },
+    //         FloatingInteractionText {
+    //             offset: Vec3::new(0.0, 14.0, 0.0),
+    //             trigger_distance: 35.0,
+    //             key_prompt: "E".to_string(),
+    //         },
+    //     ));
+    // });
     commands.entity(character_entity).with_children(|parent| {
         parent.spawn((
             SpriteBundle {
+                texture: asset_server.load("pixelorama/indicator_blink.png"),
                 sprite: Sprite {
-                    color: Color::BLACK,
-                    custom_size: Some(Vec2::new(12.0, 12.0)), // TODO: find a way to add a border radius
+                    custom_size: Some(Vec2::new(5.0, 5.0)),
+                    rect: Some(Rect::new(0.0, 0.0, 5.0, 5.0)), // Start with first frame
                     ..default()
                 },
-                transform: Transform::from_xyz(0.0, 14.0, 0.0),
-                visibility: Visibility::Hidden,
-                ..default()
-            },
-            FloatingInteractionText {
-                offset: Vec3::new(0.0, 12.0, 0.0),
-                trigger_distance: 35.0,
-                key_prompt: "E".to_string(),
-            },
-        ));
-        parent.spawn((
-            Text2dBundle {
-                text: Text::from_section(
-                    "E",
-                    TextStyle {
-                        font: asset_server.load("fonts/OpenSans-Regular.ttf"),
-                        font_size: 12.0,
-                        color: Color::WHITE,
-                    },
-                ),
-                transform: Transform::from_xyz(0.0, 14.0, 1.0),
+                transform: Transform::from_xyz(8.0, 8.0, 1.0), // Offset to right and above character
                 visibility: Visibility::Hidden,
                 ..default()
             },
@@ -97,12 +188,13 @@ pub fn spawn_interaction_indicator(
                 trigger_distance: 35.0,
                 key_prompt: "E".to_string(),
             },
+            InteractionIndicator::default(),
         ));
     });
 }
 
 impl Plugin for MiscObjectsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, update_interaction_indicators);
+        app.add_systems(Update, (update_interaction_indicators, animate_interaction_indicators));
     }
 }
